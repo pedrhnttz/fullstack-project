07c4fced4b948125a302002e05b18d57dc92d31d
Lucas Eduardo <lucas.eduardo0112@outlook.com>
2025-09-16 17:03:37 -0300

    tentando arrumar erro de codigo sensivel

--- src/Infrastructure/http/whatsapp.py
+++ src/Infrastructure/http/whatsapp.py
@@ -1,6 +1,20 @@
+import random
+import os
+from twilio.rest import Client
+
 class WhatsApp:
     def whatsApp():
-        #usar funcao randomica 
-        # imtegracao
-        token = 12343
-        return token 
+        account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        content_sid='HX229f5a04fd0510ce1b071852155d3e75',
+        content_variables='{"1":"409173"}',
+        to='whatsapp:+5511960539593'
+        )
+
+        print(message.sid)
+    def criacao_token():
+        return f"{random.randint(0, 9999):04d}"
