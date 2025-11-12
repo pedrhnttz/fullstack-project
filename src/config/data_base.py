ce84f5b68a56b9a218a794efe0f611a1c7b32e76
Pedro Santos <pedrohenos600@gmail.com>
2025-11-12 16:32:37 -0300

    test fix render

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -4,26 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-USER = os.getenv("user")
-PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
-
-DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
-
 db = SQLAlchemy()
 
 def init_db(app):
-    global DATABASE_URL
+    os.environ.pop("DATABASE_URL", None)
+    
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    PORT = os.getenv("port")
+    DBNAME = os.getenv("dbname")
 
-    if DATABASE_URL and "psycopg2" in DATABASE_URL:
-        DATABASE_URL = DATABASE_URL.replace("psycopg2", "psycopg")
+    DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+    os.environ["DATABASE_URL"] = DATABASE_URL
+
     db.init_app(app)
 
-    os.environ["DATABASE_URL"] = DATABASE_URL
 
