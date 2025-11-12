48742c957eee88f36396992027aa170b4b60905a
Pedro Santos <pedrohenos600@gmail.com>
2025-11-11 23:23:12 -0300

    database up to supabase

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -2,17 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 
 def init_db(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:senha@localhost:3306/market_management"
+    load_dotenv()
+
+    DATABASE_URL = os.getenv("SUPABASE_URL")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+    engine = create_engine(DATABASE_URL)
     if not database_exists(engine.url):
         create_database(engine.url)
 
