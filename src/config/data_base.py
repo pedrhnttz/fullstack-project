c156c0f8bb41ee7796582ee5fd752f0aa22aa362
Pedro Santos <pedrohenos600@gmail.com>
2025-11-12 15:36:01 -0300

    test fix render

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -1,15 +1,20 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 load_dotenv()
 
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+
 db = SQLAlchemy()
 
 def init_db(app):
-    DATABASE_URL = os.getenv("DATABASE_URL")
-
     if DATABASE_URL and "psycopg2" in DATABASE_URL:
         DATABASE_URL = DATABASE_URL.replace("psycopg2", "psycopg")
 
