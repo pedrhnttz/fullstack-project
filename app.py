8a56efa2a426cbdd06ba52fc5932ba689478e962
Pedro Santos <pedrohenos600@gmail.com>
2025-11-12 15:52:38 -0300

    test fix render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from src.config.data_base import init_db, db
 from src.routes import init_routes
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config["JWT_SECRET_KEY"] = "plt" 
     jwt = JWTManager(app)
     init_db(app)
