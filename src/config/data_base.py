be479bdfff76c43663b5eda4882e664be31be0d5
Pedro Santos <pedrohenos600@gmail.com>
2025-11-13 19:11:06 -0300

    render database update credentials

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -9,7 +9,12 @@ db = SQLAlchemy()
 def init_db(app):
     os.environ.pop("DATABASE_URL", None)
     
-    DATABASE_URL = os.getenv("dburl")
+    USER = os.getenv("user")
+    PASSWORD = os.getenv("password")
+    HOST = os.getenv("host")
+    DBNAME = os.getenv("dbname")
+
+    DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{HOST}/{DBNAME}'
 
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
